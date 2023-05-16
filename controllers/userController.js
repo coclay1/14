715d75bec1aeba168911875bb21adad5e025da2e
coc lay <coc.lay97@gmail.com>
2023-05-15 22:26:51 -0400

    add user api routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,44 +4,78 @@ const { User, Thought } = require('../models');
 module.exports = {
     getUsers(req, res) {
         User.find()
-        .then(async (users) => {
-            const userObj = {
-                users,
-            };
-            return res.json(userObj);
-        })
-        .catch((err) => {
-            console.log(err);
-            return res.status(500).json(err)
-        })
+            .then(async (users) => {
+                const userObj = {
+                    users,
+                };
+                return res.json(userObj);
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err)
+            })
     },
     getSingleUser(req, res) {
         User.findOne({ _id: req.params.id })
-        .then(async (user) =>
-        !user
-        ?res.status(404).json({ message: 'No User Found!'})
-        : res.json({
-            user,
-        })
-        .catch((err) => {
-            console.log(err);
-            return res.status(500).json(err)
-        }));
+            .then(async (user) =>
+                !user
+                    ? res.status(404).json({ message: 'No User Found!' })
+                    : res.json({
+                        user,
+                    })
+                        .catch((err) => {
+                            console.log(err);
+                            return res.status(500).json(err)
+                        }));
     },
     newUser(req, res) {
         User.create(req.body)
-        .then((user) => res.json(user))
-        .catch((err) => res.status(500).json(err))
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err))
     },
     deleteUser(req, res) {
-        User.findOneAndRemove({ _id: req.params.studentId })
-        .then((student) => 
-        !student
-        ? res.status(500).json({ message: "No User Found" })
-        : res.json({message: "Delete Successful!"}))
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err)
-        });
+        User.findOneAndRemove({ _id: req.params.id })
+            .then((student) =>
+                !student
+                    ? res.status(500).json({ message: "No User Found" })
+                    : res.json({ message: "Delete Successful!" }))
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err)
+            });
     },
-}
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.id },
+            { $set: req.body })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No User Found' })
+                    : res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+    addFriend(req, res) {
+        Student.findOneAndUpdate(
+            { _id: req.params.studentId },
+            { $addToSet: { friends: req.body } }
+                .then((user) =>
+                    !user
+                        ? res.status(404).json({ message: 'No User Found' })
+                        : res.json(user))
+                .catch((err) => res.status(500).json(err))
+        );
+    },
+    removeFriend(req, res) {
+        Student.findOneAndUpdate(
+            { _id: req.params.studentId },
+            { $pull: { friends: { ObjectId: req.params.id } } }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No User Found' })
+                    : res.json(user))
+            .catch((err) => res.status(500).json(err))
+            ;
+    }
+};
+
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,4 +1,22 @@
 const router = require('express').Router();
 
+const {
+    getUsers,
+    getSingleUser,
+    newUser,
+    deleteUser,
+    updateUser,
+    addFriend,
+    removeFriend,
+} = require('../../controllers/userController')
+
+// api/users
+router.route('/users').get(getUsers).post(newUser);
+// api single user
+router.route('/users/:id').get(getSingleUser).delete(deleteUser).put(updateUser)
+// api user friends
+router.route('/users/:id/friends').post(addFriend);
+router.route('/users/:id/friends/:id').delete(removeFriend);
+
 
 module.exports = router;
